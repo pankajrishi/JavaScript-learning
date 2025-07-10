b72c60aafbb9a1603ae957112fd09a2fa6fc47da
Pankaj Rishi <149893235+pankajrishi@users.noreply.github.com>
2025-07-10 06:28:30 +0000

    data type done

---
+++ 01_Basics/2_datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+alert(3+3) //we are using nodejs, not browser
+
+let name="pankaj"
+let age=20
+let isloggedIn=false
+
+
+//number 2 to power 53
+// bigint
+//string=> ""
+// boolean =>true/false
+// null =>standalone value
+// symbol =>unique
+
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
