7c748073019da0e695b799754aa8b146576efa0e
Pankaj Rishi <149893235+pankajrishi@users.noreply.github.com>
2025-07-10 07:50:39 +0000

    data type conversion

---
+++ 01_Basics/3_datatypeConversion.js
@@ -0,0 +1,28 @@
+let score="pankaj"
+let valueInNumber=Number(score)
+
+//console.log(score)
+//console.log(typeof score)
+
+//console.log(valueInNumber) //NaN
+//console.log(typeof valueInNumber)  //number
+
+// "33" =>33
+//"33abc" =>NaN
+//true =>1,false=>0
+
+let isloggedIn="pankaj"
+let booleanIsLoggedIn=Boolean(isloggedIn)
+
+//1=>tre; 0=>false
+//""=>false
+//"pankaj"=>tre
+
+let someNumber=33
+let stringNumber=String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
